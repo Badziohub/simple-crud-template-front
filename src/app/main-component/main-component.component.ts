39bce526b16c8dc5f5e7a8bc7ef58b5995b64841
Badziohub <blazejbadzio@gmail.com>
2023-07-18 17:25:23 +0200

    sorting done!

--- src/app/main-component/main-component.component.ts
+++ src/app/main-component/main-component.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -7,10 +7,13 @@ import {MatListModule} from "@angular/material/list";
 import {NgForOf, NgIf} from "@angular/common";
 import {ɵEmptyOutletComponent} from "@angular/router";
 import {Item} from "./Item";
-import {MatTableModule} from "@angular/material/table";
+import {MatTable, MatTableModule} from "@angular/material/table";
 import {AddingComponent} from "../adding/adding.component";
 import {FormsModule} from "@angular/forms";
 import {EditingComponent} from "../editing/editing.component";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {min} from "rxjs";
 
 
 
@@ -19,7 +22,7 @@ import {EditingComponent} from "../editing/editing.component";
   templateUrl: './main-component.component.html',
   styleUrls: ['./main-component.component.scss'],
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatListModule, NgForOf, ɵEmptyOutletComponent, NgIf, MatTableModule, AddingComponent, FormsModule,EditingComponent],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatListModule, NgForOf, ɵEmptyOutletComponent, NgIf, MatTableModule, AddingComponent, FormsModule, EditingComponent, MatFormFieldModule, MatSelectModule],
 })
 
 export class MainComponentComponent implements OnInit{
@@ -28,15 +31,33 @@ export class MainComponentComponent implements OnInit{
   constructor(private httpConfig: HttpConfig) {
   }
 
+  @ViewChild(MatTable) table: MatTable<any> | undefined;
   addingShown : boolean = false;
   editingShown : boolean = false;
   requests : number = 0;
+  sorting : string = '';
   items : Item[] = [];
   pickedId : number | undefined;
   displayedColumns: string[] = ['id', 'content', 'status','edit', 'delete'];
   ngOnInit() {
     this.getItems();
     this.getCounter();
+  }
+
+  sortItems() {
+    if(this.sorting == 'idAsc'){
+      this.items = this.items.sort((a,b) => a.id < b.id ? -1 : 1);
+      this.table?.renderRows();
+    } else if(this.sorting == 'idDesc') {
+      this.items = this.items.sort((a,b) => a.id > b.id ? -1 : 1);
+      this.table?.renderRows();
+    } else if(this.sorting == 'statusDesc') {
+      this.items = this.items.sort((a,b) => a.status > b.status ? -1 : 1);
+      this.table?.renderRows();
+    } else if(this.sorting == 'statusAsc') {
+      this.items = this.items.sort((a,b) => a.status < b.status ? -1 : 1);
+      this.table?.renderRows();
+    }
   }
 
   toggleAdding(){
@@ -69,6 +90,7 @@ export class MainComponentComponent implements OnInit{
   }
 
   getItems():void{
+    this.sorting = 'none';
     this.httpConfig.getItems().subscribe(
       res => {
           this.items = res as Item[];
